c77f81afed6168560bb02ee2e72c479ea1c5b8e2
seal0112 <seal0112@hotmail.com>
2018-05-02 16:15:56 +0800

    Buttons

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,6 +17,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { FavoriteService } from './services/favorite.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
         DishService,
         PromotionService,
         LeaderService,
-        ProcessHTTPMsgService
+        ProcessHTTPMsgService,
+        FavoriteService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
+import { FavoriteService } from '../services/favorite.service';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
 import 'rxjs/add/operator/switchMap';
+import { Toasty } from 'nativescript-toasty';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,18 +19,40 @@ export class DishdetailComponent implements OnInit {
   dish: Dish;
   comment: Comment;
   errMess: string;
+  avgstars: string;
+  numcomments: number;
+  favorite: boolean;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
+    private favoriteservice: FavoriteService,
+    private fonticon: TNSFontIconService,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
 
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => this.dish = dish,
+      .subscribe(dish => {
+          this.dish = dish;
+          this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+          this.numcomments = this.dish.comments.length;
+
+          let total = 0;
+          this.dish.comments.forEach(comment => total += comment.rating);
+          this.avgstars = (total/this.numcomments).toFixed(2);
+        },
           errmess => { this.dish = null; this.errMess = <any>errmess; });
+  }
+
+  addToFavorites() {
+    if (!this.favorite) {
+      console.log('Adding to Favorites', this.dish.id);
+      this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      toast.show();
+    }
   }
 
   goBack(): void {
